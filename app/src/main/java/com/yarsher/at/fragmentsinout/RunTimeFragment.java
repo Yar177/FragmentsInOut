3b4a177e48b3d81471bc42d9af0fc4a63cdec57f
Yar <yarsher.dev@gmail.com>
2019-03-07 10:56:48 -0500

    runTimeFragments

---
+++ app/src/main/java/com/yarsher/at/fragmentsinout/RunTimeFragment.java
@@ -0,0 +1,13 @@
+package com.yarsher.at.fragmentsinout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RunTimeFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run_time_fragment);
+    }
+}
