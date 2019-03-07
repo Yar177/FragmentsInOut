6f948bab340c87467e8ec514563e5e5f2c24f13f
Yar <yarsher.dev@gmail.com>
2019-03-07 12:50:37 -0500

    cleanup

--- app/src/main/java/com/yarsher/at/fragmentsinout/FragmentActivityCommunication.java
+++ app/src/main/java/com/yarsher/at/fragmentsinout/FragmentActivityCommunication.java
@@ -3,10 +3,13 @@ package com.yarsher.at.fragmentsinout;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.yarsher.at.fragmentsinout.fragments.SendMessageFragment;
 
-public class FragmentActivityCommunication extends AppCompatActivity {
+public class FragmentActivityCommunication extends AppCompatActivity implements SendMessageFragment.OnMessageSentListener {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +28,15 @@ public class FragmentActivityCommunication extends AppCompatActivity {
                     .add(R.id.message_fragment_container, sendMessageFragment, null);
 
             fragmentTransaction.commit();
-
-
-
-
         }
-
-
     }
+
+    @Override
+    public void onMessageSent(String string) {
+        textView = (TextView) findViewById(R.id.tv_message_display);
+        textView.append("\n" + string);
+        //textView.setText(string);
+    }
+
+
 }
--- app/src/main/java/com/yarsher/at/fragmentsinout/fragments/SendMessageFragment.java
+++ app/src/main/java/com/yarsher/at/fragmentsinout/fragments/SendMessageFragment.java
@@ -1,11 +1,16 @@
 package com.yarsher.at.fragmentsinout.fragments;
 
 
+import android.app.Activity;
+import android.content.Context;
+import android.location.OnNmeaMessageListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.yarsher.at.fragmentsinout.R;
 
@@ -13,6 +18,15 @@ import com.yarsher.at.fragmentsinout.R;
  * A simple {@link Fragment} subclass.
  */
 public class SendMessageFragment extends Fragment {
+
+    private EditText editText;
+    private Button button;
+
+    OnMessageSentListener onMessageSentListener;
+
+    public interface OnMessageSentListener{
+        public void onMessageSent(String string);
+    }
 
 
     public SendMessageFragment() {
@@ -24,7 +38,34 @@ public class SendMessageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_send_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_send_message, container, false);
+        editText = view.findViewById(R.id.tvMessage);
+        button = view.findViewById(R.id.btnSendMessage);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = editText.getText().toString();
+                onMessageSentListener.onMessageSent(message);
+            }
+        });
+
+        return view;
     }
 
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        Activity activity = (Activity) context;
+        try {
+            onMessageSentListener = (OnMessageSentListener) activity;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
 }
