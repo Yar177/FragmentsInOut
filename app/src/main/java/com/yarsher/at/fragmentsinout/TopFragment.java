369b9765f8edffaa1196bc367d659ca30ec8bc95
Yar <yarsher.dev@gmail.com>
2019-03-07 10:21:16 -0500

    Initial commit

---
+++ app/src/main/java/com/yarsher/at/fragmentsinout/TopFragment.java
@@ -0,0 +1,19 @@
+package com.yarsher.at.fragmentsinout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.style.TtsSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View topFragment = inflater.inflate(R.layout.top_fragment, container, false);
+        return topFragment;
+    }
+}
